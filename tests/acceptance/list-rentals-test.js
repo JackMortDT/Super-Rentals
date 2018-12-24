6d8ea6e9c7222a5f672305853d95f29d2083396c
LuisSas <luis@makingdevs.com>
2018-12-23 21:45:05 -0600

    Adding application goals as tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,9 +5,22 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function(assert) {
-    await visit('/');
-
-    assert.equal(currentURL(), '/');
+  test('Should show rentals as the home page', async function(assert){
   });
+
+  test('Should link to information about the company', async function(assert){
+  });
+
+  test('Should link to contact information', async function(assert){
+  });
+
+  test('Should list available rentals', async function(assert){
+  });
+
+  test('Should filter the list of rentals by city', async function(assert){
+  });
+
+  test('Should show details for a selected rental', async function(assert){
+  });
+
 });
