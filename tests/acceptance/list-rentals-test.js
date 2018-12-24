6e0fcba913a2ff545f5b8be437aa15ee4d4666b3
LuisSas <luis@makingdevs.com>
2018-12-24 01:29:18 -0600

    Implementing application tests for rentals, about and contact page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,17 +1,25 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { click, currentURL, visit } from '@ember/test-helpers'
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
   test('Should show rentals as the home page', async function(assert){
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'Should redirect automatically');
   });
 
   test('Should link to information about the company', async function(assert){
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'Should navigate to contact');
   });
 
   test('Should link to contact information', async function(assert){
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'Should navigate to about');
   });
 
   test('Should list available rentals', async function(assert){
